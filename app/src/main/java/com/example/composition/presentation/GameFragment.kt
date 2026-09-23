8b5102fc9c1b26ddef18be7e29da0686c3e790e8
anton <korolyov.anton0208@gmail.com>
2026-09-23 12:38:33 +0300

    Parcelable explore

--- app/src/main/java/com/example/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -44,7 +44,9 @@ class GameFragment : Fragment() {
     }
 
     private fun parseArgs() {
-        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+        requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
+            level = it
+        }
     }
 
     companion object {
@@ -56,7 +58,7 @@ class GameFragment : Fragment() {
         fun newInstance(level: Level): GameFragment {
             return GameFragment().apply {
                 arguments = Bundle().apply {
-                    putSerializable(KEY_LEVEL, level)
+                    putParcelable(KEY_LEVEL, level)
                 }
             }
         }
